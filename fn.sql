015217fbe43e8882f53e74ea41074431f007c986
sjf <395195766@qq.com>
2019-01-25 16:08:04 +0800

    数据库修改

--- fn.sql
+++ fn.sql
@@ -7,13 +7,13 @@ USE fn;
 #--2 创建数据表
 #--2-1 管理员信息表
 CREATE TABLE  xfn_admin(
-    aid INT,   #--管理员编号
+    aid INT PRIMARY KEY AUTO_INCREMENT,  #--管理员编号
     aname VARCHAR(32) UNIQUE,  #--管理员用户名
     apwd VARCHAR(64)     #--管理员密码
 );
  #--2-2 项目全局设置
 CREATE TABLE xfn_settings(
-    sd INT PRIMARY KEY,   #--编号
+    sd INT PRIMARY KEY AUTO_INCREMENT,   #--编号
     appName VARCHAR(32),    #--引用、店家名称
     apiUrl VARCHAR(64),    #--数据api子系统地址
     AdminUrl VARCHAR(64),  #--管理后台子系统地址
@@ -23,14 +23,14 @@ CREATE TABLE xfn_settings(
 );
  #--2-3 桌台信息表
 CREATE TABLE xfn_table(
-    tid  INT PRIMARY KEY,   #--桌台编号
+    tid  INT PRIMARY KEY AUTO_INCREMENT,   #--桌台编号
     tname VARCHAR(64),    #--桌台昵称
     tpe VARCHAR(16),    #--桌台类型
     stus INT
 );
  #--2-4 桌台预定信息表
 CREATE TABLE xfn_reservation(
-    rid INT PRIMARY KEY,   
+    rid INT PRIMARY KEY AUTO_INCREMENT,   
     contactName VARCHAR(64),
     phone VARCHAR(16),       #--联系电话
     contactTime BIGINT,     # --联系时间
@@ -38,12 +38,12 @@ CREATE TABLE xfn_reservation(
 );
  #--2-5 菜品分类
 CREATE TABLE xfn_category(
-    cid INT PRIMARY KEY,  
+    cid INT PRIMARY KEY AUTO_INCREMENT,  
     cname  VARCHAR(32)
 );
  #--2-6 菜品信息表
 CREATE TABLE xfn_dish(
-    did INT PRIMARY KEY,    #--菜品编号
+    did INT PRIMARY KEY AUTO_INCREMENT,    #--菜品编号
     title VARCHAR(32),      #--菜品名称
     imgUrl VARCHAR(128),    #--图片地址
     price DECIMAL(6,2),      #--价格
@@ -53,7 +53,7 @@ CREATE TABLE xfn_dish(
 );
 #--2-7 订单表
 CREATE TABLE xfn_order(
-    oid INT PRIMARY KEY,  
+    oid INT PRIMARY KEY AUTO_INCREMENT,  
     startTime BIGINT,
     endTime BIGINT,   
     customerCount INT,   #用餐人数
@@ -62,7 +62,7 @@ CREATE TABLE xfn_order(
 );
 #--2-8  订单详细信息表
 CREATE TABLE xfn_order_detail(
-    did INT PRIMARY KEY,
+    did INT PRIMARY KEY AUTO_INCREMENT,
     dishId INT,   #菜品编号
     FOREIGN KEY (dishId) REFERENCES xfn_dish (did),   #外键
     dishCount INT,  #菜品数量
@@ -72,6 +72,6 @@ CREATE TABLE xfn_order_detail(
 );
 #管理用户插入信息
 INSERT INTO xfn_admin VALUES
-(1,'SJF',123),
-(2,'Different',123),
-(3,'Infinite',123);
+(null,'SJF',PASSWORD('123')),
+(null,'Different',PASSWORD('123')),
+(null,'Infinite',PASSWORD('123'));
